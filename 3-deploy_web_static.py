d9ef89a3dea74e157fd7a258382a5c9296835281
Hafida <hafidabelaidagnaoui@gmail.com>
2023-12-08 18:10:30 +0000

    ip

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.171.213', '184.73.99.187']
 
 
 def do_pack():
